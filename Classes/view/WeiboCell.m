6720538490efa14c6844b7608c5ddc8bc16a131d
poorevil <poorevil@gmail.com>
2013-11-26 20:26:01 +0800

    update ui

--- Classes/view/WeiboCell.m
+++ Classes/view/WeiboCell.m
@@ -8,6 +8,11 @@
 
 #import "WeiboCell.h"
 
+#import "WaveTransMetadata.h"
+#import "EGOImageView.h"
+
+#import "VdiskJSON.h"
+
 @implementation WeiboCell
 
 - (id)initWithFrame:(CGRect)frame
@@ -27,5 +32,25 @@
     // Drawing code
 }
 */
+
+-(void)setMetadata:(WaveTransMetadata *)metadata
+{
+    super.metadata = metadata;
+    
+    NSDictionary *jsonDict = [metadata.content JSONValue];
+    self.nameLabel.text = [jsonDict objectForKey:@"name"];
+    self.descriptionLabel.text = [jsonDict objectForKey:@"description"];
+    
+    self.headerImageView.imageURL = [NSURL URLWithString:[jsonDict objectForKey:@"avatar_large"]];
+}
+
+-(void)dealloc
+{
+    self.headerImageView = nil;
+    self.nameLabel = nil;
+    self.descriptionLabel = nil;
+    
+    [super dealloc];
+}
 
 @end
