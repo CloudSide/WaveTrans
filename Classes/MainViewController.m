1cfe55d71ec64bd94c44f59d22ef515803a4eddd
poorevil <poorevil@gmail.com>
2013-11-26 17:43:27 +0800

    update ui

--- Classes/MainViewController.m
+++ Classes/MainViewController.m
@@ -41,19 +41,19 @@
     
     self.metadataList = [WaveTransModel metadataList];
     
-    if (self.metadataList.count ==0) {
-        
-        for (int i = 0; i<10; i++) {
-            WaveTransMetadata *wt = [[WaveTransMetadata alloc] initWithSha1:[NSString stringWithFormat:@"356a192b7913b04c54574d18c28d46e6395428a%d",i]
-                                                                       type:@"file"
-                                                                    content:@"http://sdfsdf"
-                                                                       size:1212
-                                                                   filename:@"av.mp3"];
-            [wt save];
-            [wt release];
-        }
-        self.metadataList = [WaveTransModel metadataList];
-    }
+//    if (self.metadataList.count ==0) {
+//        
+//        for (int i = 0; i<10; i++) {
+//            WaveTransMetadata *wt = [[WaveTransMetadata alloc] initWithSha1:[NSString stringWithFormat:@"356a192b7913b04c54574d18c28d46e6395428a%d",i]
+//                                                                       type:@"file"
+//                                                                    content:@"http://sdfsdf"
+//                                                                       size:1212
+//                                                                   filename:@"av.mp3"];
+//            [wt save];
+//            [wt release];
+//        }
+//        self.metadataList = [WaveTransModel metadataList];
+//    }
     
     self.mTableView = [[UITableView alloc] initWithFrame:self.view.frame];
     self.mTableView.delegate = self;
