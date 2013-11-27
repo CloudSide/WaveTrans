561fd8daee8f17031d0dfb5d8c4489cd28af09ab
poorevil <poorevil@gmail.com>
2013-11-27 15:30:18 +0800

    add PhotoViewerViewController

---
+++ Classes/Controller/PhotoViewerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewerViewController.h
+//  WaveTrans
+//
+//  Created by hanchao on 13-11-27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewerViewController : UIViewController
+
+@end
---
+++ Classes/Controller/PhotoViewerViewController.m
@@ -0,0 +1,38 @@
+//
+//  PhotoViewerViewController.m
+//  WaveTrans
+//
+//  Created by hanchao on 13-11-27.
+//
+//
+
+#import "PhotoViewerViewController.h"
+
+@interface PhotoViewerViewController ()
+
+@end
+
+@implementation PhotoViewerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
